4c0305d933f88d123e89fe2e101c8aa796b9618d
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2024-05-08 01:29:08 -0400

    small fix

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,7 @@
 import { createApolloServer } from '@/server'
 
 const bootstrap = async () => {
-  const { url } = await createApolloServer({ port: 4000 })
+  const { url } = await createApolloServer({ port: 3000 })
   console.log(`GraphQL server ready at ${url}`)
 }
 
