90ecbbc0540e8cf83be0f6fb2c83c6fc7d8e311f
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2024-05-18 17:54:58 -0400

    move port to config

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,5 @@
 export const config = {
+  app: { port: 3005 },
   jwt: { secret: 'secret', exp: '1y' },
   admin: { email: 'admin@mail.mock' },
 }
--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,8 @@
+import { config } from '@/config'
 import { createApolloServer } from '@/server'
 
 const bootstrap = async () => {
-  const { url } = await createApolloServer({ port: 3000 })
+  const { url } = await createApolloServer({ port: config.app.port })
   console.log(`GraphQL server ready at ${url}`)
 }
 
